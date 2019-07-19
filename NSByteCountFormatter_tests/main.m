43a5ee66d21f2a0542902c633236ee897ccdd3fa
Gregory John Casamento <greg.casamento@gmail.com>
2019-07-19 05:56:38 -0400

    Fix tests to reflect correct names for things being tested

--- NSByteCountFormatter_tests/main.m
+++ NSByteCountFormatter_tests/main.m
@@ -57,17 +57,25 @@ void runTests()
     output = [NSByteCountFormatter stringFromByteCount:1024 * 1024
                                             countStyle:NSByteCountFormatterCountStyleMemory];
     NSLog(@"%@",output);
-    passTest([output isEqualToString: @"1 MB"], @"NSByteCountFormatterCountStyleFile works");
+    passTest([output isEqualToString: @"1 MB"], @"NSByteCountFormatterCountStyleMemory works");
 
     output = [NSByteCountFormatter stringFromByteCount:1024 * 1024
                                             countStyle:NSByteCountFormatterCountStyleDecimal];
     NSLog(@"%@",output);
-    passTest([output isEqualToString: @"1 MB"], @"NSByteCountFormatterCountStyleFile works");
+    passTest([output isEqualToString: @"1 MB"], @"NSByteCountFormatterCountStyleDecimal works");
 
     output = [NSByteCountFormatter stringFromByteCount:1024 * 1024
                                             countStyle:NSByteCountFormatterCountStyleBinary];
     NSLog(@"%@",output);
-    passTest([output isEqualToString: @"1 MB"], @"NSByteCountFormatterCountStyleFile works");
+    passTest([output isEqualToString: @"1 MB"], @"NSByteCountFormatterCountStyleBinary works");
+    
+    NSByteCountFormatter *formatter = [[NSByteCountFormatter alloc] init];
+    NSData *data = [NSKeyedArchiver archivedDataWithRootObject:formatter];
+    BOOL flag = [data writeToFile:@"output.data" atomically:YES];
+    if (!flag)
+    {
+        NSLog(@"Error writing");
+    }
 }
 
 int main(int argc, const char * argv[]) {
